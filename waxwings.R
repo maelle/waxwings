41864570e83d512e97e4e41e4c04444cc266483f
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-03-29 20:39:56 +0200

    1st commit

---
+++ waxwings.R
@@ -0,0 +1,84 @@
+library("spocc")
+library("dplyr")
+library("purrr")
+library("scrubr")
+library("ggplot2")
+library("ggalt") 
+library("magick")
+library("ggmap")
+library("ggthemes")
+############################################################
+#                                                          #
+#                         get data                         ####
+#                                                          #
+############################################################
+
+get_slice <- function(longitude){
+  print(longitude)
+  gbifopts <- list(limit = 200000,
+                   year = 2016)
+  inatopts <- list(maxresults = 100000, year = 2016)
+  waxwings <- occ(query = "Bombycilla garrulus",
+                  from = c('gbif', 'inat', 'ebird'), 
+                  gbifopts = gbifopts,
+                  inatopts = inatopts,
+                  geometry = c(longitude, - 90, longitude + 5, 90))
+  waxwings <- occ2df(waxwings)
+}
+longitudes <- seq(-180, 175, by = 5)
+waxwings <- lapply(longitudes, get_slice)
+
+############################################################
+#                                                          #
+#                          clean                           ####
+#                                                          #
+############################################################
+for (i in 1:length(waxwings)){
+  waxwings[[i]]$latitude <- as.numeric(waxwings[[i]]$latitude)
+  waxwings[[i]]$longitude <- as.numeric(waxwings[[i]]$longitude)
+}
+
+waxwings <- bind_rows(waxwings)
+waxwings <- unique(waxwings)
+waxwings <- split(waxwings, 
+                  lubridate::week(waxwings$date))
+
+cleanup <- function(df){
+  print(df$date[1])
+  df %>%
+    coord_impossible() %>%
+    coord_incomplete() %>%
+    coord_unlikely() %>%
+    dedup() %>%
+    date_standardize("%Y-%m-%d") %>%
+    date_missing()
+} 
+
+waxwings <- lapply(waxwings, cleanup)
+
+waxwings <- bind_rows(waxwings)
+waxwings <- unique(waxwings)
+readr::write_csv(waxwings, path = "waxwings.csv")
+nrow(waxwings)
+############################################################
+#                                                          #
+#                           map                            ####
+#                                                          #
+############################################################
+wax_map <- map_data("world")
+wax_map <- wax_map[wax_map$region != "Antarctica",]
+p <- ggplot()
+p <- p + geom_map(data = wax_map,
+           map = wax_map,
+           aes(x = long, y = lat, map_id = region),
+           color = "white", fill = "#7f7f7f",
+           size = 0.05, alpha = 1/4)
+p <- p + theme_map() 
+p <- p + geom_point(aes(longitude, latitude,
+                 col = lubridate::month(date)),
+             data = waxwings)
+p <- p + ylim(0, 80)
+p + theme(legend.position = "none")
+
+ggsave(p, "map.png",
+       width = 8, height = 8)
