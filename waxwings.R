360d3b511787ab2b1992edf0c4098cf06841ee90
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-03-29 21:10:44 +0200

    starts looking at both seasons

--- waxwings.R
+++ waxwings.R
@@ -84,3 +84,41 @@ p + theme(legend.position = "none")
 
 ggsave(p, file = "map.png",
        width = 8, height = 8)
+
+############################################################
+#                                                          #
+#                        second map                        ####
+#                                                          #
+############################################################
+
+waxwings <- readr::read_csv("waxwings.csv")
+
+waxwings <- mutate(waxwings,
+                   breeding = 
+                     lubridate::month(date) %in%
+                     c(3:9))
+wax_map <- map_data("world")
+wax_map <- wax_map[wax_map$region != "Antarctica",]
+
+p <- ggplot()
+p <- p + geom_map(data = wax_map,
+                  map = wax_map,
+                  aes(x = long, y = lat, map_id = region),
+                  color = "white", fill = "#7f7f7f",
+                  size = 0.05, alpha = 1/4)
+p <- p + theme_map() 
+p <- p + geom_point(aes(longitude, latitude),
+                    data = waxwings,
+                    size = .5)
+p <- p + ylim(40, 80)
+p <- p + facet_grid(breeding ~ .) +
+  ggtitle("Breeding season (March-Sept)")
+p +  geom_bkde2d(bandwidth=c(5, 5),
+                 aes(longitude, latitude),
+                 data = waxwings[waxwings$breeding,],
+                 col = "red")+  
+  geom_bkde2d(bandwidth=c(5, 5),
+            aes(longitude, latitude),
+            data = waxwings[!waxwings$breeding,],
+            col = "blue")
+
