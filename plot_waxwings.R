1916c43a2f4d6eca8ffaa33ccefaf4c51c26c824
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-04-02 16:15:18 +0200

    progress

--- plot_waxwings.R
+++ plot_waxwings.R
@@ -4,6 +4,7 @@ library("magick")
 library("ggmap")
 library("ggthemes")
 library("dplyr")
+library("albersusa")
 waxwings <- readr::read_csv("waxwings.csv")
 waxwings <- filter(waxwings, name != "Bombycilla japonica",
                    longitude < 50)
@@ -34,41 +35,68 @@ ggsave(p, file = "map.png",
 
 ############################################################
 #                                                          #
+#                     day of the week                      ####
+#                                                          #
+############################################################
+waxwings <- mutate(waxwings, 
+                   wday = lubridate::wday(date, label = TRUE))
+waxwings %>%
+  group_by(update(date, wday = 1), wday) %>%
+  summarize(n = n()) %>%
+ggplot() +
+  geom_boxplot(aes(wday, n))
+
+ggsave(file = "wday.png", width = 8, height = 6)
+############################################################
+#                                                          #
 #                        second map                        ####
 #                                                          #
 ############################################################
-
-waxwings <- readr::read_csv("waxwings.csv")
-
 waxwings <- mutate(waxwings,
-                   breeding = 
-                     lubridate::month(date) %in%
-                     c(3:9),
                    month = lubridate::month(date))
-wax_map <- map_data("world")
-wax_map <- wax_map[wax_map$region != "Antarctica",]
+cedrorum <- filter(waxwings,
+                   name == "Bombycilla cedrorum")
+plot_month_cedrorum <- function(df, cedrorum){
+  p <- ggplot()
+  p <- p + ggtitle(df$month[1])
+  p <- p + geom_map(data = wax_map,
+                    map = wax_map,
+                    aes(x = long, y = lat, map_id = region),
+                    color = "white", fill = "#7f7f7f",
+                    size = 0.05, alpha = 1/4)
+  p <- p + theme_map() 
+  p <- p + geom_point(aes(longitude, latitude),
+                      data = df,
+                      size = .5,
+                      col = "red")
+  p <- p + ylim(min(cedrorum$latitude), 
+                max(cedrorum$latitude))
+  p <- p + xlim(min(cedrorum$longitude), 
+                max(cedrorum$longitude))
+  outfil <- paste0("cedrorum_", df$month[1], ".png")
+  ggsave(outfil, p, width=5, height=5)
+  
+  outfil
+  
+}
 
-p <- ggplot()
-p <- p + geom_map(data = wax_map,
-                  map = wax_map,
-                  aes(x = long, y = lat, map_id = region),
-                  color = "white", fill = "#7f7f7f",
-                  size = 0.05, alpha = 1/4)
-p <- p + theme_map() 
-p <- p + geom_point(aes(longitude, latitude),
-                    data = waxwings,
-                    size = .5)
-p <- p + ylim(40, 80)
-p <- p + facet_grid(month ~ name) +
-  ggtitle("Breeding season (March-Sept)")
-p <- p +  geom_bkde2d(bandwidth=c(5, 5),
-                      aes(longitude, latitude),
-                      data = waxwings[waxwings$breeding,],
-                      col = "red")+  
-  geom_bkde2d(bandwidth=c(5, 5),
-              aes(longitude, latitude),
-              data = waxwings[!waxwings$breeding,],
-              col = "blue")
+cedrorum_l <- split(cedrorum, cedrorum$month)
 
-ggsave(p, file = "map2.png",
-       width = 20, height = 5)
+
+cedrorum_l %>%
+  map(plot_month_cedrorum, cedrorum = cedrorum) %>%
+  map(image_read) %>%
+  image_join() %>%
+  image_animate(fps=1) %>%
+  image_write("cedrorum.gif")
+
+garrulus <- filter(waxwings,
+                   name == "Bombycilla garrulus")
+garrulus_l <- split(garrulus, garrulus$month)
+
+garrulus_l %>%
+  map(plot_month_cedrorum, cedrorum = garrulus) %>%
+  map(image_read) %>%
+  image_join() %>%
+  image_animate(fps=1) %>%
+  image_write("garrulus.gif")
